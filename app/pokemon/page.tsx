776185be71965b91614cafc1f31fd5960d62678a
Jhon Mario <jhond2648@gmail.com>
2025-08-02 22:10:13 -0400

    Update: se quito la cantidad de pokemones a obtener  para obtener la lista completa

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -2,7 +2,7 @@ import ListPokemons from '@/components/pokemon/PokemonList';
 import { getEnhancedPokemons } from '@/services/pokeapi';
 
 export default async function PokemonList() {
-  const pokemons = await getEnhancedPokemons(400);
+  const pokemons = await getEnhancedPokemons();
   
     return (
       <div >
