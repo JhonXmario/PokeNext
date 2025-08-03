42551ae915dda6c0f135f17e2b7bafd0d50e24a3
Jhon Mario <jhond2648@gmail.com>
2025-08-02 22:09:17 -0400

    Refactor: Se movio el componente Navbar del layout al page principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/ui/HeroSection';
 import FooterSection from '@/components/ui/FooterSection';
+import Navbar from "@/components/ui/Navbar";
 export default async function PokemonList() {
 
   return (
     <div >
+      <Navbar />
       <HeroSection />
       <FooterSection />
     </div>
