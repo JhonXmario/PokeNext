5a301e575d95af81f4c046055122757d10525962
Jhon Mario <jhond2648@gmail.com>
2025-08-31 19:49:22 -0400

    Added: Definición de interfaces para detalles de Pokémon, especies, cadena de evolución y estadísticas

---
+++ types/pokemon-details-types.ts
@@ -0,0 +1,86 @@
+export interface PokemonDetail {
+  id: number
+  name: string
+  height: number
+  weight: number
+  sprites: {
+    front_default: string
+    front_shiny: string
+    other: {
+      home: {
+        front_default: string
+        front_shiny: string
+      }
+      "official-artwork": {
+        front_default: string
+        front_shiny: string
+      }
+    }
+  }
+  types: Array<{
+    type: {
+      name: string
+    }
+  }>
+  abilities: Array<{
+    ability: {
+      name: string
+    }
+    is_hidden: boolean
+  }>
+  stats: Array<{
+    base_stat: number
+    stat: {
+      name: string
+    }
+  }>
+  species: {
+    url: string
+  }
+}
+
+export interface PokemonSpecies {
+  flavor_text_entries: Array<{
+    flavor_text: string
+    language: {
+      name: string
+    }
+  }>
+  evolution_chain: {
+    url: string
+  }
+  genera: Array<{
+    genus: string
+    language: {
+      name: string
+    }
+  }>
+}
+
+export interface EvolutionChain {
+  chain: {
+    species: {
+      name: string
+    }
+    evolves_to: Array<{
+      species: {
+        name: string
+      }
+      evolves_to: Array<{
+        species: {
+          name: string
+        }
+      }>
+    }>
+  }
+}
+
+export interface PokemonStats {
+  hp: number
+  attack: number
+  defense: number
+  specialAttack: number
+  specialDefense: number
+  speed: number
+  total: number
+}
